0dac1ceac31a892118cbe47f31e065859b3fbd7a
armenkevorkian <c4074060@ncl.ac.uk>
2025-08-21 13:29:28 +0300

    progess from video

--- deploy-django/src/cfehome/settings.py
+++ deploy-django/src/cfehome/settings.py
@@ -9,24 +9,41 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+
+from decouple import config
+from django.core.management.utils import get_random_secret_key
+
+
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+REPO_DIR=BASE_DIR.parent
+TEMPLATES_DIR = BASE_DIR / "templates"
+print(BASE_DIR , REPO_DIR , TEMPLATES_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str , default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANO_DEBUG") == "true"
+DEBUG = config("DJANGO_DEBUG",cast=bool, default=False) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+   #"hungrypy.com"
+    #"www.hungrypy.com"
+    ".railway.app"
+]
 
+CSRF_TRUSTED_ORIGINS =  [
+"https://*.railway.app"
+]
+
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -54,7 +71,7 @@ ROOT_URLCONF = 'cfehome.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

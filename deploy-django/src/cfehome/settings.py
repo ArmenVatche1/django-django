c9848854cbd9dfb4841bd5a427984b168c6cd043
armenkevorkian <c4074060@ncl.ac.uk>
2025-09-08 11:02:11 +0300

    updated var and project name

--- deploy-django/src/cfehome/settings.py
+++ deploy-django/src/cfehome/settings.py
@@ -12,17 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from decouple import config
 from django.core.management.utils import get_random_secret_key
-
-
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 REPO_DIR=BASE_DIR.parent
 TEMPLATES_DIR = BASE_DIR / "templates"
-print(BASE_DIR , REPO_DIR , TEMPLATES_DIR)
-
+TEMPLATES_DIR.mkdir(parents=True , exist_ok=True)
+PROJECT_NAME = config("PROJECT_NAME", default= "Unset Project Name")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
